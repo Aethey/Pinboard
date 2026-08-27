68b403bc8c4f7700c29855255ea8b0d7af54fcdd
Aethey <work369741@gmail.com>
2026-08-27 18:56:48 +0900

    Initial Commit

---
+++ Pinboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pinboard
+//
+//  Created by Ry@ on 2026/08/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pinboard/PinboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinboardApp.swift
+//  Pinboard
+//
+//  Created by Ry@ on 2026/08/27.
+//
+
+import SwiftUI
+
+@main
+struct PinboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
